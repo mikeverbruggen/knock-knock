095460101a77e50a2825e4c184ed663801da4dbf
mikeverbruggen <mike@slmmr.nl>
2017-03-12 17:50:40 +0100

    Update index.php

--- wordpress/wp-content/themes/knock-knock/index.php
+++ wordpress/wp-content/themes/knock-knock/index.php
@@ -1,36 +1,524 @@
-<?php get_header(); ?>
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-<section id="grid-system">
+    <title>Carousel Template for Bootstrap</title>
 
-	<div class="page-header">
-		<h1>Berichten (index)</h1>
-	</div>
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
 
-	<div class="row">
-		<!-- Content -->
-		<div class="span8">
-			
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-			get_template_part( 'content', get_post_format() );
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 
-		endwhile; ?>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <col-md- class="sr-only">Toggle navigation</col-md->
+                <col-md- class="icon-bar"></col-md->
+                <col-md- class="icon-bar"></col-md->
+                <col-md- class="icon-bar"></col-md->
+              </button>
+              <a class="navbar-brand" href="#">Klopvaart Intranet</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+	
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">Agenda</a></li>
+                <li><a href="#contact">Documentatie</a></li>
+                <li><a href="#about">Bewoners</a></li>
+              </ul>
+
+		    <ul class="nav navbar-nav navbar-right">	
+			      <li class="dropdown search-dropdown">
+			        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Zoeken <col-md- class="caret"></col-md-></a>
+			        <ul class="dropdown-menu">
+			          <li><form class="navbar-form navbar-right">
+			            <input type="text" class="form-control" placeholder="Search...">
+			          </form></li>
+			        </ul>
+			      </li>
+		      <li class="dropdown">
+		        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mike Verbruggen<col-md- class="caret"></col-md-></a>
+		        <ul class="dropdown-menu">
+		          <li><a href="#">Action</a></li>
+		          <li><a href="#">Another action</a></li>
+		          <li><a href="#">Something else here</a></li>
+		          <li role="separator" class="divider"></li>
+		          <li class="dropdown-header">Nav header</li>
+		          <li><a href="#">Separated link</a></li>
+		          <li><a href="#">One more separated link</a></li>
+		        </ul>
+		      </li>
+		    </ul>
+
+
+
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+		<div class="jumbotron">
+	      <div class="container">
+	        <h1>Documentatie</h1>
+	      </div>
+	    </div>
+
+		<div class="container page">
+
+		<section id="documentation">
+
+			<div class="row">
+
+			<div class="col-md-8 overview">
+
+					<div class="message">
+					<div class="message-header">
+						<h3>Algemeen</h3>
+					</div>
+					<div class="message-body">
+						<div class="f_rijtje"><ul class="overview">
+							<li><i class="icon-file"></i><a href="/documentatie/adres-vereniging">Adres vereniging</a> </li>
+							<li><i class="icon-file"></i><a href="/documentatie/contributie">Contributie</a> </li>
+							<li><i class="icon-file"></i><a href="/documentatie/declareren">Declareren</a> </li></ul>
+						</div>
+
+						<div class="f_rijtje"><ul class="overview">
+							<li><i class="icon-file"></i><a href="/documentatie/e-mailadressen-van-de-vereniging">E-mailadressen van de vereniging</a> </li>
+							<li><i class="icon-file"></i><a href="/documentatie/rondzendmail">Rondzendmail</a> </li>
+							<li><i class="icon-file"></i><a href="/documentatie/wie-doet-wat-lijst">Wie-doet-wat lijst</a> </li></ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="message">
+					<div class="message-header">
+						<h3>Bestuur</h3>
+					</div>
+					<div class="message-body">
+
+					<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/financien">Financiën</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/notulen-alv">Notulen ALV</a> </li></ul>
+		</div>
+
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/notulen-bestuursvergadering">Notulen bestuursvergadering</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/omgaan-met-conflicten">Omgaan met conflicten</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/statuten">Statuten</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/vereniging">Vereniging</a> </li></ul>
+						</div>
+					</div>
+				</div>
+
+					<div class="message">
+					<div class="message-header">
+						<h3>Commissies</h3>
+					</div>
+					<div class="message-body">
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/activiteiten">Activiteiten</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/barcommissie">Barcommissie</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/beheercommissie">Beheercommissie</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/commissies">Commissies</a>  <col-md- style="color:black;">Concept</col-md-></li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/klopper">De Klopper</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/eetcafe">Eetcafé</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/filmcommissie">Filmcommissie</a> </li></ul>
+		</div>
+
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/internetcommissie">Internetcommissie</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/kascommissie">Kascommissie</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/tuincommissie">Tuincommissie</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/voco">VoCo</a>  <col-md- style="color:black;">Concept</col-md-></li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/wachtlijstcommissie">Wachtlijstcommissie</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/welkommissie">Welkommissie</a> </li></ul>
+		</div>
+
+
+					</div>
+				</div>
+
+
+
+
+					<div class="message">
+					<div class="message-header">
+						<h3>Faciliteiten</h3>
+					</div>
+					<div class="message-body">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/bakfiets">Bakfiets</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/beamer">Beamer</a>  <col-md- style="color:black;">Concept</col-md-></li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/boormachine">Boormachine</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/dartboard">Dartboard</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/pooltafel">Pooltafel</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/projectruimte">Projectruimte</a> </li></ul>
+		</div>
+
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/sinterklaasspullen">Sinterklaasspullen</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/speeltoestellen">Speeltoestellen</a>  <col-md- style="color:black;">Concept</col-md-></li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/steekwagen">Steekwagen</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/stroommeter">Stroommeter</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/tafeltennistafel">Tafeltennistafel</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/tuinhok">Tuinhok</a> </li></ul>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+					</div>
+				</div>
+
+
+
+
+					<div class="message">
+					<div class="message-header">
+						<h3>Huur en nieuwe huurders</h3>
+					</div>
+					<div class="message-body">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/doorverhuizen">Doorverhuizen</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/Groter-of-kleiner-wonen-in-je-huis">Groter of kleiner wonen in je huis</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/Huisbewaarderschap">Huisbewaarderschap</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/huiscontract">Huiscontract</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/huurtoeslag">Huurtoeslag</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/inkomensgrens1">Inkomensgrens</a> </li></ul>
+		</div>
+
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/aanmeldformulier">Op zoek naar een nieuwe bewoner</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/opzeggen-van-de-huur">Opzeggen van de huur</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/portaal">Portaal</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/samenwerkingsovereenkomst-met-portaal">Samenwerkingsovereenkomst met Portaal</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/Samenwonen">Samenwonen</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/Tips-voor-de-gesprekken-met-kandidaten">Tips voor de gesprekken met kandidaten</a> </li></ul>
+		</div>
+					</div>
+				</div>
+
+
+
+
+					<div class="message">
+					<div class="message-header">
+						<h3>In, op en om het huis</h3>
+					</div>
+					<div class="message-body">
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/afspraken-over-vuur-en-geluid-in-de-tuin">Afspraken over vuur en geluid in de tuin</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/boiler">Boiler</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/douche">Douche</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/ean-codes-van-de-huizen">EAN-codes van de huizen</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/hoofdkraan-afsluiten">Hoofdkraan afsluiten</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/inbraakpreventie">Inbraakpreventie</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/radiatoren">Radiatoren</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/rookmelders">Rookmelders</a> </li></ul>
+		</div>
+
+		<div class="f_rijtje"><ul class="overview">
+
+				<li><i class="icon-file"></i><a href="/documentatie/schuiframen-gebruiksaanwijzing">Schuiframen gebruiksaanwijzing</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/tl-buisjes">TL-buisjes</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/trappenhuis-schilderen">Trappenhuis schilderen</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/ventilatie">Ventilatie</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/verwarmingsketel">Verwarmingsketel</a>  <col-md- style="color:black;">Concept</col-md-></li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/vloerbedekking-trappenhuizen">Vloerbedekking trappenhuizen</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/wc">WC</a> </li>
+
+				<li><i class="icon-file"></i><a href="/documentatie/zonneboiler">Zonneboiler</a> </li></ul>
+		</div>
+
+					</div>
+				</div>
+
+
+
+
+					<div class="message">
+					<div class="message-header">
+						<h3>Overige</h3>
+					</div>
+					<div class="message-body">
+
+		<div class="f_rijtje"><ul class="overview">
+				<li><i class="icon-file"></i><a href="/documentatie/gegevens-oud-bewoners">Gegevens oud-bewoners</a> </li></ul>
+		</div>
 		
-		<nav>
-			<ul class="pager">
-				<li><?php next_posts_link( 'Vorige' ); ?></li>
-				<li><?php previous_posts_link( 'Volgende' ); ?></li>
-			</ul>
-		</nav>
-		
-		<?php endif; ?>
+					</div>
+				</div>
+			</div>
 
-		</div><!-- /Content -->
+			<div class="col-md-4">
 
-<?php get_sidebar(); ?>
+				<div class="message">
+					<div class="message-header">
+					<h3>Laatst gewijzigd</h3>
+					</div>
+					<div class="message-body">
+					<ul class="overview">
 
-	</div>
+					<li><i class="icon-file"></i><a href="/documentatie/wie-doet-wat-lijst">Wie-doet-wat lijst</a></li>
 
-</section>
+					<li><i class="icon-file"></i><a href="/documentatie/internetcommissie">Internetcommissie</a></li>
 
-<?php get_footer(); ?>
+					<li><i class="icon-file"></i><a href="/documentatie/notulen-bestuursvergadering">Notulen bestuursvergadering</a></li>
+
+					<li><i class="icon-file"></i><a href="/documentatie/samenwerkingsovereenkomst-met-portaal">Samenwerkingsovereenkomst met Portaal</a></li>
+
+					<li><i class="icon-file"></i><a href="/documentatie/rondzendmail">Rondzendmail</a></li>
+
+					<li><i class="icon-file"></i><a href="/documentatie/trappenhuis-schilderen">Trappenhuis schilderen</a></li>
+
+					<li><i class="icon-file"></i><a href="/documentatie/onderhoudswerkzaamheden-chronologisch">Onderhoudswerkzaamheden chronologisch</a></li>
+
+					<li><i class="icon-file"></i><a href="/documentatie/test1">test</a></li>
+
+					<li><i class="icon-file"></i><a href="/documentatie/boekjaar-2015">boekjaar 2015</a></li>
+
+					<li><i class="icon-file"></i><a href="/documentatie/financien">Financiën</a></li>
+
+					</ul>
+					</div>
+				</div>
+
+
+
+
+				<div class="message">
+					<div class="message-header">
+					<h3>Documentenbeheer</h3>
+					<p>Deze 10 documenten zijn als laatste bewerkt en vallen onder jou <a href="/documentatie/beheer">beheer</a>.</p>
+					</div>
+					<div class="message-body">
+					<ul class="overview">
+
+					<li><i class="icon-file"></i><a href="/documentatie/klopvaart.nl-email-adressen">Klopvaart.nl email adressen</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/alv-notulisten-overzicht">ALV notulisten overzicht</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/bakfiets">Bakfiets</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/draaiboek-alv">Draaiboek ALV</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/commissies">Commissies</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/vacaturelijst">Vacaturelijst</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/activiteiten">Activiteiten</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/licenties">Software Licenties</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/intranet-wensenlijstjes">Intranet wensenlijstjes</a></li>
+
+
+
+
+
+					<li><i class="icon-file"></i><a href="/documentatie/beamer">Beamer</a></li>
+
+								</ul>
+					</div>
+				</div>
+
+
+
+
+			</div>
+
+		</div>
+
+		</section>
+
+
+		     <!-- Footer
+		      ================================================== -->
+		      <footer class="footer">
+		        <p class="pull-right"><a href="#">Back to top</a></p>
+		        <p>Heb je vragen of opmerkingen? Laat het de webmeester weten. Mail naar <a href="mailto:webmeester@klopvaart.nl">webmeester@klopvaart.nl</a></p>
+		      </footer>
+
+		    </div>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
