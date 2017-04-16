769753281c40e3fe70c58863d74587e11db35e26
mikeverbruggen <mike@slmmr.nl>
2017-04-16 20:51:24 +0200

    Added Custom Templates

---
+++ wordpress/wp-content/themes/knock-knock/page-agenda.php
@@ -0,0 +1,147 @@
+<?php /* Template Name: Agenda */ ?>
+
+<?php get_header(); ?>
+	
+	<div class="jumbotron">
+      <div class="container">
+        <h1><?php wp_title(''); ?></h1>
+      </div>
+    </div>
+
+<div class="container">
+	
+  <div class="row">
+    <div class="col-8">
+	
+		<div class="content-box content-box-info" role="content-box">
+		  	<h4 class="content-box-heading">Agenda</h4>
+		  	<div class="mb-0 clearfix">
+
+				<div class="docs-list col-6">
+
+					<ul class="overview">
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/financien">Financiën</a> </li>
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/notulen-alv">Notulen ALV</a> </li>
+
+					</ul>
+
+				</div>
+
+				<div class="docs-list col-6">
+
+					<ul class="overview">
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/financien">Financiën</a> </li>
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/notulen-alv">Notulen ALV</a> </li>
+
+					</ul>
+
+				</div>
+
+			</div>
+		</div>
+	
+		<div class="list-group">
+		  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+		    <div class="d-flex w-100 justify-content-between">
+		      <h5 class="mb-1"><i class="fa fa-camera-retro fa-1x"></i> Update 1</h5>
+		      <small>1 days ago</small>
+		    </div>
+		    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+		    <small>At lacinia metus felis ut turpis.</small>
+		  </a>
+		  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+		    <div class="d-flex w-100 justify-content-between">
+		      <h5 class="mb-1">Update 2</h5>
+		      <small class="text-muted">4 days ago</small>
+		    </div>
+		    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+		    <small>At lacinia metus felis ut turpis.</small>
+		  </a>
+		  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+		    <div class="d-flex w-100 justify-content-between">
+		      <h5 class="mb-1">Update 3</h5>
+		      <small class="text-muted">8 days ago</small>
+		    </div>
+		    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+		    <small>At lacinia metus felis ut turpis.</small>
+		  </a>
+		  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+		    <div class="d-flex w-100 justify-content-between">
+		      <h5 class="mb-1">Update 4</h5>
+		      <small class="text-muted">10 days ago</small>
+		    </div>
+		    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+		    <small>At lacinia metus felis ut turpis.</small>
+		  </a>
+
+		</div>	
+		
+		<hr>
+
+		<div class="card text-center">
+		  <div class="card-header">
+		    Aankondiging
+		  </div>
+		  <div class="card-block">
+		    <h4 class="card-title">Algemene ledenvergadering</h4>
+		    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		    <a href="#" class="btn btn-primary">Ik ben er bij!</a>
+		  </div>
+		  <div class="card-footer text-muted">
+		    2 days ago
+		  </div>
+		</div>
+
+		<hr>
+
+
+
+    </div>
+    <div class="col">
+
+		<ul class="list-group">
+		  <li class="list-group-item justify-content-between">
+		   <i class="fa fa-camera-retro fa-1x"></i> Cras justo odio
+		    <span class="badge badge-default badge-pill">14</span>
+		  </li>
+		  <li class="list-group-item justify-content-between">
+		    Dapibus ac facilisis in
+		    <span class="badge badge-default badge-pill">2</span>
+		  </li>
+		  <li class="list-group-item justify-content-between">
+		    Morbi leo risus
+		    <span class="badge badge-default badge-pill">1</span>
+		  </li>
+		</ul>
+		
+		<hr>
+
+		<div class="content-box content-box-info" role="content-box">
+		  <h4 class="content-box-heading">Laatst gewijzigd</h4>
+		  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel condimentum massa, eu egestas lectus. Morbi at tortor arcu. Nunc feugiat viverra risus nec porta. Maecenas imperdiet nec libero vitae mollis. Morbi tristique diam at risus dignissim auctor. Donec viverra diam in lectus semper vulputate. Pellentesque pretium libero id risus imperdiet egestas. Ut ultricies nibh in volutpat porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed commodo, neque at mattis congue, ligula arcu pulvinar orci, eu sodales turpis tellus mattis tellus. Etiam pretium, quam non interdum semper, nisl tellus ornare massa, at lacinia metus felis ut turpis.</p>
+		</div>
+		<div class="content-box content-box-info" role="content-box">
+		  <h4 class="content-box-heading">Documentenbeheer</h4>
+		  <p class="mb-0">This is Knock Knock, the first Cohousing Intranet Wordpress Theme.</p>
+		</div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/knock-knock/page-documentation.php
@@ -0,0 +1,122 @@
+<?php /* Template Name: Documentation */ ?>
+
+<?php get_header(); ?>
+	
+	<div class="jumbotron">
+      <div class="container">
+        <h1><?php wp_title(''); ?></h1>
+      </div>
+    </div>
+
+<div class="container">
+	
+  <div class="row">
+    <div class="col-8">
+	
+		<div class="content-box content-box-info" role="content-box">
+		  	<h4 class="content-box-heading">Algemeen</h4>
+		  	<div class="mb-0 clearfix">
+				<div class="docs-list col-6">
+					<ul class="overview">
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/financien">Financiën</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/notulen-alv">Notulen ALV</a> </li>
+					</ul>
+				</div>
+				<div class="docs-list col-6">
+					<ul class="overview">
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/financien">Financiën</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/notulen-alv">Notulen ALV</a> </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<div class="content-box content-box-info" role="content-box">
+		  	<h4 class="content-box-heading">Algemeen</h4>
+		  	<div class="mb-0 clearfix">
+				<div class="docs-list col-6">
+					<ul class="overview">
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/financien">Financiën</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/notulen-alv">Notulen ALV</a> </li>
+					</ul>
+				</div>
+				<div class="docs-list col-6">
+					<ul class="overview">
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/financien">Financiën</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/notulen-alv">Notulen ALV</a> </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<div class="content-box content-box-info" role="content-box">
+		  	<h4 class="content-box-heading">Algemeen</h4>
+		  	<div class="mb-0 clearfix">
+				<div class="docs-list col-6">
+					<ul class="overview">
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/financien">Financiën</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/notulen-alv">Notulen ALV</a> </li>
+					</ul>
+				</div>
+				<div class="docs-list col-6">
+					<ul class="overview">
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/algemene-ledenvergadering">Algemene Ledenvergadering</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/bestuur">Bestuur 2015/16</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/financien">Financiën</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/huishoudelijk-reglement">Huishoudelijk Reglement</a> </li>
+						<li><i class="fa fa-file-text"></i> <a href="/documentatie/notulen-alv">Notulen ALV</a> </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+    </div><!-- /col-8 -->
+
+
+    <div class="col">
+
+		<ul class="list-group">
+		  <li class="list-group-item justify-content-between">
+		   <i class="fa fa-camera-retro fa-1x"></i> Cras justo odio
+		    <span class="badge badge-default badge-pill">14</span>
+		  </li>
+		  <li class="list-group-item justify-content-between">
+		    Dapibus ac facilisis in
+		    <span class="badge badge-default badge-pill">2</span>
+		  </li>
+		  <li class="list-group-item justify-content-between">
+		    Morbi leo risus
+		    <span class="badge badge-default badge-pill">1</span>
+		  </li>
+		</ul>
+		
+		<hr>
+
+		<div class="content-box content-box-info" role="content-box">
+		  <h4 class="content-box-heading">Laatst gewijzigd</h4>
+		  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel condimentum massa, eu egestas lectus. Morbi at tortor arcu. Nunc feugiat viverra risus nec porta. Maecenas imperdiet nec libero vitae mollis. Morbi tristique diam at risus dignissim auctor. Donec viverra diam in lectus semper vulputate. Pellentesque pretium libero id risus imperdiet egestas. Ut ultricies nibh in volutpat porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed commodo, neque at mattis congue, ligula arcu pulvinar orci, eu sodales turpis tellus mattis tellus. Etiam pretium, quam non interdum semper, nisl tellus ornare massa, at lacinia metus felis ut turpis.</p>
+		</div>
+		<div class="content-box content-box-info" role="content-box">
+		  <h4 class="content-box-heading">Documentenbeheer</h4>
+		  <p class="mb-0">This is Knock Knock, the first Cohousing Intranet Wordpress Theme.</p>
+		</div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
